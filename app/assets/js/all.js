399ef8dfb5974caf703028e1a0ea59d121029d76
rio wei <wutony0713@gmail.com>
2022-08-12 11:49:29 +0800

    第一頁完成 + JS

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,17 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+const reply =document.querySelector(`#reply`);
+const close = document.querySelector(`#close`);
+const edit  = document.querySelector(`#edit`);
+const answer = document.querySelector(`#answer`);
+const outBtn = document.querySelector(`#outBtn`);
+// 打開文字編輯框
+reply.addEventListener(`click`,() =>{
+  answer.classList.add("d-none");
+  outBtn.classList.add("d-none");
+  edit.classList.remove("d-none");
+})
+// 關閉文字編輯框
+close.addEventListener(`click`,() =>{
+  edit.classList.add(`d-none`);
+  answer.classList.remove("d-none");
+  outBtn.classList.remove("d-none");
+})
